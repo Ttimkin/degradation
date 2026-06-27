5b01499be7ca87d349acdc0d9afcc082f3c3e07c
Ttimkin <tignatenok@mail.ru>
2026-06-27 14:14:16 +0300

    Добавил Spring и подключил БД

--- auth_server/src/main/java/auth/server/Main.java
+++ auth_server/src/main/java/auth/server/Main.java
@@ -1,7 +1,30 @@
 package auth.server;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        // auth
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:sqlite:authDB.db");
+        } catch (SQLException hui) {
+            System.err.println("DB not found");
+            hui.printStackTrace();
+        }
+
+        
+
+        try {
+            conn.close();
+        } catch (SQLException err) {
+            err.printStackTrace();
+        }
     }
 }
